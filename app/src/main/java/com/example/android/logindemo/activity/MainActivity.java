b40b2dc87cdc24cd243f47d9379438b6e072e219
parikshith shetty <ab007shetty@gmail.com>
2022-07-10 10:27:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/logindemo/activity/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.android.logindemo.activity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android.logindemo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private TextView forgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.email);
+        Password = (EditText) findViewById(R.id.password);
+        Login = (Button) findViewById(R.id.btn_login);
+        userRegistration = (TextView) findViewById(R.id.btn_signup);
+        forgotPassword = (TextView) findViewById(R.id.tvForgotPassword);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null) {
+            finish();
+            startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        }
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PasswordActivity.class));
+            }
+        });
+    }
+
+    private void validate(String userName, String userPassword) {
+
+        progressDialog.setMessage("Connecting to server...");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    checkEmailVerification();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+
+                    progressDialog.dismiss();
+
+                }
+            }
+        });
+
+
+    }
+
+    private void checkEmailVerification() {
+        startActivity(new Intent(MainActivity.this, SecondActivity.class));
+    }
+
+}
